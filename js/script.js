589eb814da3e1581876f0928641653362e3839ad
Yanzhe Wu <wuy420@newschool.edu>
2019-04-17 15:11:32 -0400

    API

--- js/script.js
+++ js/script.js
@@ -1,24 +1,12 @@
 
 
-
-
 var music = ["Youngblood", "Lucid Dreams", "Shallow", "Girls Like You", "Happier", "Better Now"];
-// var music = ["music1", "music2", "music3", "music4", "music5", "music6"];
 var random = 0;
 var back = 0;
-// var audioElement;
-// var sound;
-// var pause = false;
 
 function randomNum(arrayName){
 	return Math.floor(Math.random()*arrayName.length);
 }
-
-// function playMusic(r){
-// 	console.log("playmusic");
-// 	audioElement.setAttribute('src', "music/"+music[r]+".mp3");
-// 	audioElement.play();
-// }
 
 function playMusic(r){
 
@@ -31,29 +19,11 @@ function playMusic(r){
 
 	$('#switch').click(function(){
 		sound.stop();
-		// pause = false;
 	});
 
 	sound.once('load', function(){
 		sound.play();
 	});
-
-	// sound.on('end', function(){
- //  		console.log('Finished!');
-	// });
-	
-	// $('#control').click(function(){
-	// 	if(pause){
-	// 		pause = false;
-	// 		sound.play();
-	// 		$('#control').css("background", "url('cover/pause.png') no-repeat");
-	// 	}else{
-	// 		sound.pause();
-	// 		pause = true;
-	// 		$('#control').css("background", "url('cover/play.png') no-repeat");
-	// 	}
-	// });
-
 }
 
 function changeCover(){
@@ -64,14 +34,12 @@ function changeCover(){
 	$('#cover').css("background", "url('cover/" + music[random] + ".png') no-repeat");
 	$('#cover').css("background-size", "500px 500px");
 	console.log("url('cover/" + music[random] + ".png') no-repeat");
-	// $('#control').css("display", "block");
-	
 }
 
 function changeBackground(){
 	if(back == 0){
 		console.log("black");
-		$('#back').css("background-color", "#0c1a27");
+		$('#back').css("background-color", "#85adad");
 		$('#title').css("color", "white");
 		back = 1;
 	} else if(back == 1){
@@ -80,20 +48,38 @@ function changeBackground(){
 		$('#title').css("color", "#0c1a27");
 		back = 0;
 	}
+
 }
+
+function changeBackgroundImage(){
+	$.ajax({
+			url: "https://api.unsplash.com/photos/random",
+			method: "GET",
+			dataType: "json",
+			headers: {
+   				"Authorization": "Client-ID dff55246a193c9d14d513c0cdde1ac11958055b2533c08df372c298e9dd8720d",
+  			},
+  			success: function(data){
+  				var imgurl = data.urls.full
+  				$('#bg').attr("src", imgurl);
+  			}
+	})
+}
+
 
 
 function init(){
 	console.log("init success");
-	// audioElement = document.createElement('audio');
-	// audioElement.setAttribute('autoplay', 'autoplay');
 	$("#switch").click(function(){
 		changeCover();
+		changeBackground();
+		changeBackgroundImage();
 	});
-	$("body").click(function(){
+	$("#back").click(function(){
 		changeBackground();
 		console.log("back clicked");
 	});
+	
 }
 
 $(document).ready(function(){
